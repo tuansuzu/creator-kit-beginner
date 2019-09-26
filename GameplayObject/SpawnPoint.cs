d54ccec3d68ad70f4a19f6ce6ff695ca11f81518
tuansuzu <mr.buiminhtuan@yahoo.com>
2019-09-26 09:41:32 +0700

    Add files via upload

---
+++ GameplayObject/SpawnPoint.cs
@@ -0,0 +1,30 @@
+﻿using CreatorKitCodeInternal;
+using UnityEngine;
+
+namespace CreatorKitCode 
+{
+    /// <summary>
+    /// When the player walk through the trigger on the same GameObject as that script, the GameObject position become
+    /// the new place where the player will respawn when their health reach 0.
+    /// </summary>
+    public class SpawnPoint : MonoBehaviour
+    {
+        void OnTriggerEnter(Collider other)
+        {
+            //no need to check as only the player can trigger object in that layer, if it create a null ref it SHOULD
+            //error as that mean this other object shouldn't be in that layer
+            CharacterControl chr = other.GetComponent<CharacterControl>();
+            chr.SetNewRespawn(this);
+        }
+
+        public void Activated()
+        {
+            enabled = false;
+        }
+
+        public void Deactivated()
+        {
+            enabled = true;
+        }
+    }
+}
---
+++ VFXDatabase.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace CreatorKitCode 
+{
+    /// <summary>
+    ///This allow to define a list of VFX prefab with a name.
+    ///An editor script take care of generating a C# file containing an enum with the given name to index the Entries array
+    ///so you can do in code something like VFXManager.SpawnFX(MyEffectName) 
+    /// </summary>
+    [CreateAssetMenu(fileName = "VFXDatabase", menuName = "Beginner Code/VFXDatabase", order = -800)]
+    public class VFXDatabase : ScriptableObject
+    {
+        [System.Serializable]
+        public class VFXDBEntry
+        {
+            public string Name;
+            public GameObject Prefab;
+            public int PoolSize = 6;
+        }
+
+        public VFXDBEntry[] Entries;
+    }
+}
